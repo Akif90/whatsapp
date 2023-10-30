ecd8ad961b3ac93a17976af424cbf4abf6742ef2
Akif Ahmed <akifahmed90@gmail.com>
2023-10-30 23:58:44 +0530

    Created Input components , contexts and reducer

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,3 +1,5 @@
+import {useStateProvider} from "@/context/StateContext";
+import {reducerCases} from "@/context/constants";
 import {CHECK_USER_ROUTE} from "@/utils/ApiRoutes";
 import {firebaseAuth} from "@/utils/FirebaseConfig";
 import axios from "axios";
@@ -9,6 +11,7 @@ import {FcGoogle} from "react-icons/fc";
 
 function login() {
   const router = useRouter();
+  const [{}, dispatch] = useStateProvider();
   const handleLogin = async () => {
     try {
       const provider = new GoogleAuthProvider();
@@ -16,7 +19,17 @@ function login() {
         user: {displayName: name, email, photoURL: profileImage},
       } = await signInWithPopup(firebaseAuth, provider);
       const {data} = await axios.post(CHECK_USER_ROUTE, {email});
-      if (!data.status) router.push("/onboarding");
+      if (!data.status) {
+        dispatch({
+          type: reducerCases.SET_NEW_USER,
+          newUser: true,
+        });
+        dispatch({
+          type: reducerCases.SET_USER_INFO,
+          userInfo: {name, email, profileImage, status: ""},
+        });
+        router.push("/onboarding");
+      }
     } catch (error) {
       console.log(error);
     }
--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Input from "@/components/common/Input";
+import {useStateProvider} from "@/context/StateContext";
+import Image from "next/image";
+import React, {useState} from "react";
 
 function onboarding() {
-  return <div>onboarding</div>;
+  const [{userInfo}] = useStateProvider();
+  const [name, setName] = useState(userInfo?.name || "");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("/default_avatar.png");
+  return (
+    <div className="flex items-center justify-center flex-col  text-white bg-panel-header-background h-screen w-screen">
+      <div className="flex items-center justify-center gap-2">
+        <Image src="/whatsapp.gif" alt="Whatsapp" height={300} width={300} />
+        <span className="text-7xl">Whatsapp</span>
+      </div>
+      <h2 className="text-2xl">Create your profile</h2>
+      <div className="flex gap-6 mt-6">
+        <div className="flex flex-col items-center justify-center mt-5 gap-6">
+          <Input name="Display Name" state={name} setState={setName} label />
+          <Input name="About" state={about} setState={setAbout} label />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default onboarding;
