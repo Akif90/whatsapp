03837e6b01df6407e4c41804f5e4d48e89593108
Akif Ahmed <akifahmed90@gmail.com>
2023-11-06 01:02:39 +0530

    Crated ChatContainer comp

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function ChatContainer() {
-  return <div>ChatContainer</div>;
+  return (
+    <div className="h-[80vh] w-full flex-grow overflow-auto custom-scrollbar">
+      <div className="bg-chat-background bg-fixed h-full w-full opacity-5 inset-0 z-0"></div>
+      <div className="flex w-full">
+        <div className="flex flex-col justify-end w-full gap-1 overflow-auto"></div>
+      </div>
+    </div>
+  );
 }
 
 export default ChatContainer;
