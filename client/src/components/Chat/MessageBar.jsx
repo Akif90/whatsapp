bbcc2c2eb4d9603e15b2992cab4a5121ac946892
Akif Ahmed <akifahmed90@gmail.com>
2023-11-07 12:28:39 +0530

    Modified Prisma schema

--- client/src/components/Chat/MessageBar.jsx
+++ client/src/components/Chat/MessageBar.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import {useStateProvider} from "@/context/StateContext";
+import React, {useState} from "react";
 import {BsEmojiSmile} from "react-icons/bs";
 import {FaMicrophone} from "react-icons/fa";
 import {ImAttachment} from "react-icons/im";
 import {MdSend} from "react-icons/md";
 
 function MessageBar() {
+  const [{userInfo, currentChatUser}, dispatch] = useStateProvider();
+  const [message, setMessage] = useState("");
+
+  const sendMessage = async () => {};
   return (
     <div className="bg-panel-header-background h-20 px-4 flex items-center gap-6 relative">
       <>
@@ -21,6 +26,8 @@ function MessageBar() {
         <div className="w-full rounded-lg h-10 flex items-center">
           <input
             type="text"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
             placeholder="Type a message"
             className="bg-input-background text-sm focus:outline-none
           text-white h-10 rounded-lg px-5 py-4 w-full"
